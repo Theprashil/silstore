208b9aa721e1323dddeff81d9ba70c209c3f2dcc
theprashil <prashil_silwal@protonmail.com>
2021-06-02 08:50:49 +0545

    Added Dynamic Cart Badge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ProductList from "./components/Laptops/ProductList";
+import { CartProvider } from "./components/Contexts/cart-context";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <header>
         <NavBar />
       </header>
       <section>
         <ProductList />
       </section>
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,18 @@
 import classes from "./Cart.module.css";
 import CartIcon from "./CartIcon";
+import { useContext } from "react";
+import { CartContext } from "../Contexts/cart-context";
+
 function Cart() {
+  const [cart, setCart] = useContext(CartContext);
+  // const totalPrice = cart.reduce((acc, curr) => acc + curr.price, 0);
   return (
     <button className={classes.btn}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span className={classes.text}>Your Cart</span>
-      <span className={classes.badge}>2</span>
+      <span className={classes.badge}>{cart.length}</span>
     </button>
   );
 }
